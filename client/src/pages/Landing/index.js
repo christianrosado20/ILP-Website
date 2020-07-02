839d5a321758663437184b11dacaada88b4660b1
Adel del Valle <adel.delvalle@upr.edu>
2020-07-02 18:07:21 -0400

    Landing page first version

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { Container, Jumbotron, Carousel }  from 'react-bootstrap';
+import  { Container, Jumbotron, Carousel, Row, Col}  from 'react-bootstrap';
 import './style.scss';
 
 const Landing = () => {
@@ -21,7 +21,27 @@ const Landing = () => {
                 <div className="Section__Two">
                     <div className="Section__Two__Card"><h3 className="Section__Two__Title">About</h3></div>
                     <h2 className="Section__Two__Text">The Industrial Liaison Program of the University of Puerto Rico Mayagüez provides a platform for students, professors and companies that fosters mutually beneficial collaborations involving research, education, and community.  The program provides support to the hundreds of companies that give and work jointly to strengthen the university antes, ahora, y siempre.</h2>
-                    
+                    <h1 className="Section__Two__Offer">What we stand for</h1>
+                    <div className="Section__Two__Purpose">
+                    <Row>
+                       <Col><div className="Section__Two__Community">
+                           <h1 className="Section__Two__CommunityT">Community</h1>
+                           <hr className="Separator"/>
+                           </div>
+                           <h2 className="Section__Two__Purpose__Text">The ILP is focused on strengthening and growing our community made possible through students, faculty, and companies working together.</h2>
+                           </Col>
+                       <Col><div className="Section__Two__Community">
+                           <h1 className="Section__Two__CommunityT">Connection</h1>
+                           <hr className="Separator"/>
+                           </div>
+                           <h2 className="Section__Two__Purpose__Text">The ILP platform allows for new community connections by finding people with similar interests to engage with in dialog towards building productive collaborations.</h2></Col>
+                       <Col><div className="Section__Two__Community">
+                           <h1 className="Section__Two__CommunityT">Growth</h1>
+                           <hr className="Separator"/></div>
+                           <h2 className="Section__Two__Purpose__Text">The ILP platform strongly encourages its members to go beyond their comfort zone to explore new relationships and opportunities enabling growth through expanded horizons.</h2>
+                           </Col>
+                    </Row>
+                    </div>
                 </div>
                 
         
--- client/src/pages/Student/index.js
+++ client/src/pages/Student/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './style.scss';
+
+import { Container, Jumbotron } from 'react-bootstrap';
 
 const Student = () => {
     return (
-        <div>
-            <h1>Student Page</h1>
-        </div>
+        <Container>
+            <Jumbotron className="Jumbotro">
+                <div className="Jumbotron__overlay"></div>
+                <div className="Jumbotron__head">
+                    <h1 className="Head__title">
+                    Students
+                    </h1>
+                </div>
+            </Jumbotron>
+            </Container>
     )
 }
 
